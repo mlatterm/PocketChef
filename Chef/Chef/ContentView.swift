d9612a920e3fa4c80f025faa2ed2dc29cff07b0c
mlatterm <70703672+mlatterm@users.noreply.github.com>
2020-09-17 21:05:23 +0200

    Add files via upload

---
+++ Chef/Chef/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chef
+//
+//  Created by Max Lattermann on 9/17/20.
+//  Copyright © 2020 Max Lattermann. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
